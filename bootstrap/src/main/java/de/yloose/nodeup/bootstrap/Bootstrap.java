92c388b3b1734de624e38e22b482559e94d2958e
yloose <yloose@YanXPS>
2022-12-10 14:00:37 +0100

    Improve error handling.

--- bootstrap/src/main/java/de/yloose/nodeup/bootstrap/Bootstrap.java
+++ bootstrap/src/main/java/de/yloose/nodeup/bootstrap/Bootstrap.java
@@ -12,18 +12,19 @@ import org.update4j.Configuration;
 import org.update4j.UpdateOptions.ArchiveUpdateOptions;
 
 public class Bootstrap {
-	
-	public static final String CONFIG_PATH = "https://github.com/yloose/Nodeup/releases/download/latest/";
+
+	public static final String CONFIG_PATH = "http://github.com/yloose/Nodeup/releases/download/latest/";
 
 	public static void main(String[] args) {
-		while (true) {
-			update();
-			try {
+
+		try {
+			while (true) {
+				update();
 				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
@@ -45,49 +46,50 @@ public class Bootstrap {
 
 	public static Configuration loadConfig() {
 		String fileName = "";
-		
+
 		try {
 			fileName = "config-" + getPlatform() + ".xml";
 		} catch (UnsupportedArchitectureException e) {
 			System.err.println("Failed to get system architecture: " + e.getMessage());
 			System.exit(-1);
 		}
-		
+
 		Configuration config = null;
+		URL configUrl = null;
 		try {
-			URL configUrl = new URL(CONFIG_PATH + fileName);
+			configUrl = new URL(CONFIG_PATH + fileName);
 			Reader in = new InputStreamReader(configUrl.openStream(), StandardCharsets.UTF_8);
 			config = Configuration.read(in);
 		} catch (IOException e) {
-			System.err.println("Could not load remote config.");
-			// TODO: Handle error
+			System.err.println("Could not load remote config with config url: " + configUrl.toString());
+			throw new RuntimeException(e);
 		}
 		return config;
 	}
-	
+
 	public static String getArch() {
 		return System.getProperty("os.arch");
 	}
-	
+
 	public static String getPlatform() throws UnsupportedArchitectureException {
 		String arch = getArch();
 		switch (arch) {
-		
+
 		case "x86_64":
 			return "amd64";
 		case "amd64":
 			return "amd64";
-			
+
 		case "arm":
 			return "armv7";
-			
+
 		case "arm64":
 			return "aarch64";
 		case "aarch64":
 			return "aarch64";
 		case "armv8":
 			return "aarch64";
-		
+
 		default:
 			throw new UnsupportedArchitectureException("Current platform: " + arch + " is not supported.");
 		}
