85781278ea26350487c5ffc0d928f0d42e8cc866
yloose <yloose@YanXPS>
2022-12-10 16:10:56 +0100

    Merge branch 'main' of https://github.com/yloose/Nodeup.git into main

--- bootstrap/src/main/java/de/yloose/nodeup/bootstrap/Bootstrap.java
+++ bootstrap/src/main/java/de/yloose/nodeup/bootstrap/Bootstrap.java
@@ -13,7 +13,7 @@ import org.update4j.UpdateOptions.ArchiveUpdateOptions;
 
 public class Bootstrap {
 
-	public static final String CONFIG_PATH = "http://github.com/yloose/Nodeup/releases/download/latest/";
+	public static final String CONFIG_PATH = "https://github.com/yloose/Nodeup/releases/download/latest/";
 
 	public static void main(String[] args) {
 
